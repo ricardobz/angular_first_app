1aa892078ae604936af053e19be31d52727a4690
Ricardo Beckert <rbeckert@esss.co>
2023-05-05 17:56:48 -0300

    Add alerts example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ServersComponent } from './servers/servers.component';
 import { ServerComponent } from './server/server.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
-  declarations: [AppComponent, ServersComponent, ServerComponent],
+  declarations: [AppComponent, ServersComponent, ServerComponent, WarningAlertComponent, SuccessAlertComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
