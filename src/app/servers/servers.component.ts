4ee7ef0f651a5c5288e468d69f3477b62026e35a
Ricardo Beckert <rbeckert@esss.co>
2023-05-05 17:50:59 -0300

    Component selector options

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
+  // selector: 'app-servers', // -> <app-servers></app-servers>
+  //selector: '[app-servers]', // -> <div app-servers></div>
+  //selector: '.app-servers', // -> <div class="app-servers"></div>
   selector: 'app-servers',
-  template: '<app-server></app-server><app-server></app-server>',
+  template: ` <app-server></app-server>
+    <app-server></app-server>`,
   styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent {}
