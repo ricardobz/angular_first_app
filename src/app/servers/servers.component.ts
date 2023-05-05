18984f13d5b5c5b7a426930ae91c80df6ab3b79c
Ricardo Beckert <rbeckert@esss.co>
2023-05-05 17:44:43 -0300

    Update app component style

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,9 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  template: '<app-server></app-server><app-server></app-server>',
+  styleUrls: ['./servers.component.css'],
 })
-export class ServersComponent {
-
-}
+export class ServersComponent {}
