5c62ad64721e6ac34481d09ac9cc91f4f7416d54
Ricardo Beckert <rbeckert@esss.co>
2023-05-16 18:03:05 -0300

    Add classes markdown
    Add cheat sheets for classes tips
    Remove commented code

---
+++ CLASSES.md
@@ -0,0 +1,23 @@
+# Module 1
+
+## Selectors
+
+
+```javascript
+selector: 'app-servers'   // -> <app-servers></app-servers>
+selector: '[app-servers]' // -> <div app-servers></div>
+selector: '.app-servers'  // -> <div class="app-servers"></div>
+```
+
+
+## String interpolation
+```html
+<p>{{ title }}</p>
+```
+
+## Property binding
+
+```html
+<p [innerText]="title"></p>
+```
+
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,12 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  // selector: 'app-servers', // -> <app-servers></app-servers>
-  //selector: '[app-servers]', // -> <div app-servers></div>
-  //selector: '.app-servers', // -> <div class="app-servers"></div>
   selector: 'app-servers',
-  // template: ` <app-server></app-server>
-  //   <app-server></app-server>`,
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css'],
 })
